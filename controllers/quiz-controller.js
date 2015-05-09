8980ef36f76987d603f9a5be7499840bd3b6dc05
Jhon Diaz <dago861228@hotmail.com>
2015-05-09 10:55:08 -0500

    Despliegue de Db

--- controllers/quiz-controller.js
+++ controllers/quiz-controller.js
@@ -1,16 +1,27 @@
 'use strict'
 
+var models = require('../models/models')
+
 exports.question = function(req,res){
-	res.render('quizes/question',{pregunta:'Capital de Italia'})
+	models.Quiz.findAll().done(function(quiz){
+		res.render('quizes/question',{pregunta:quiz[0].pregunta})
+	})
+	
 }
 exports.answer = function(req,res){
 
+	
 	if(req.body.respuesta!=''){
-		if(req.body.respuesta === 'Roma'){
-			res.render('quizes/answer',{respuesta:'Correcta'})
-		}else{
-			res.render('quizes/answer',{respuesta:'Incorrecta'})
-		}
+		models.Quiz.findAll().done(function(quiz){
+			console.log(quiz[0].respuesta)
+			console.log(req.body.respuesta)
+			if(req.body.respuesta === quiz[0].respuesta){
+				res.render('quizes/answer',{respuesta:'Correcta'})
+			}else{
+				res.render('quizes/answer',{respuesta:'Incorrecta'})
+			}
+		})
+		
 		
 	}else{
 		res.send('Debe digitar algo')
